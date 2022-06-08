ae6202446ad489ab9ab4a8ac17b40d27c4296b7c
cLo <cristianolourenco319@gmail.com>
2022-06-08 13:11:47 +0100

    definindo os widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(const MyWidget());
+  runApp(const MyApp());
 }
+// no flutter tudo sao widgets, desde telas, botoes , texto...
+
+// os widgets classificam em dois tipos o stateless e o stateul
+
+// o stateless e aquele que nao possui alteracao de estado(estatico)
+
+// o statefull ao contraeio do stateless possui alteracao de estado(dinamico)
+
+// para criar um widget aqui no vs code basta digital stless/stful
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}): super(key: key);
@@ -19,17 +28,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.black,
-        alignment: Alignment.center,
-        child: const Text('O ola mundo!'),
-      ),
-    );
-  }
-}
